70170a4b27f36ee93cd86413537a65f4c99488f0
Gabriel Fontán <gabilessto@gmail.com>
2023-12-05 15:31:55 +0100

    - Añadido tailwindcsse importado a astro.config.mjs - Añadido Annotation.astro para el texto de `@Composable`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,118 +1,140 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "Jetpack Compose Pro",
-      locales: {
-        root: {
-          label: 'Español',
-          lang: 'es',
-        },
-      },      description: "La mayor base de datos de Jetpack Compose de habla hispana.",
-      editLink: {
-        baseUrl: 'https://github.com/ArisGuimera/JetpackComposePro/blob/master/'
-      },
-      social: {
-        github: "https://github.com/ArisGuimera/JetpackComposePro",
-        youtube: 'https://youtube.com/@aristidevs',
-        discord: 'https://bit.ly/3bmeQvm',
-        twitter: "https://x.com/aristidevs",
-        linkedin: 'https://www.linkedin.com/in/aristides-guimera-orozco/',
-        twitch: 'https://www.twitch.tv/aristidevs',
-      },
-      sidebar: [
-        {
-          label: "App bars",
-          autogenerate: { directory: "app-bars" },
-        },
-        {
-          label: "Badges",
-          autogenerate: { directory: "badges" },
-        },
-        {
-          label: "Buttons",
-          autogenerate: { directory: "buttons" },
-        },
-        {
-          label: "Cards",
-          autogenerate: { directory: "cards" },
-        },
-        {
-          label: "Carousel",
-          autogenerate: { directory: "carousel" },
-        },
-        {
-          label: "Checkbox",
-          autogenerate: { directory: "checkbox" },
-        },
-        {
-          label: "Chips",
-          autogenerate: { directory: "chips" },
-        },
-        {
-          label: "Pickers",
-          autogenerate: { directory: "pickers" },
-        },
-        {
-          label: "Dialogs",
-          autogenerate: { directory: "dialogs" },
-        },
-        {
-          label: "Divider",
-          autogenerate: { directory: "divider" },
-        },
-        {
-          label: "Lists",
-          autogenerate: { directory: "lists" },
-        },
-        {
-          label: "Menú",
-          autogenerate: { directory: "menu" },
-        },
-        {
-          label: "Navigation",
-          autogenerate: { directory: "navigation" },
-        },
-        {
-          label: "Progress indicator",
-          autogenerate: { directory: "progress-indicator" },
-        },
-        {
-          label: "Sheets",
-          autogenerate: { directory: "sheets" },
-        },
-        {
-          label: "Sliders",
-          autogenerate: { directory: "sliders" },
-        },
-        {
-          label: "Snackbar",
-          autogenerate: { directory: "snackbar" },
-        },
-        {
-          label: "Switch",
-          autogenerate: { directory: "switch" },
-        },
-        {
-          label: "Tabs",
-          autogenerate: { directory: "tabs" },
-        },
-        {
-          label: "Text",
-          autogenerate: { directory: "text" },
-        },
-        {
-          label: "Text fields",
-          autogenerate: { directory: "text-fields" },
-        },
-        {
-          label: "ToolTips",
-          autogenerate: { directory: "tooltips" },
-        }
-      ],
-    }),
-  ],
+  integrations: [starlight({
+    title: "Jetpack Compose Pro",
+    locales: {
+      root: {
+        label: 'Español',
+        lang: 'es'
+      }
+    },
+    description: "La mayor base de datos de Jetpack Compose de habla hispana.",
+    editLink: {
+      baseUrl: 'https://github.com/ArisGuimera/JetpackComposePro/blob/master/'
+    },
+    social: {
+      github: "https://github.com/ArisGuimera/JetpackComposePro",
+      youtube: 'https://youtube.com/@aristidevs',
+      discord: 'https://bit.ly/3bmeQvm',
+      twitter: "https://x.com/aristidevs",
+      linkedin: 'https://www.linkedin.com/in/aristides-guimera-orozco/',
+      twitch: 'https://www.twitch.tv/aristidevs'
+    },
+    sidebar: [{
+      label: "App bars",
+      autogenerate: {
+        directory: "app-bars"
+      }
+    }, {
+      label: "Badges",
+      autogenerate: {
+        directory: "badges"
+      }
+    }, {
+      label: "Buttons",
+      autogenerate: {
+        directory: "buttons"
+      }
+    }, {
+      label: "Cards",
+      autogenerate: {
+        directory: "cards"
+      }
+    }, {
+      label: "Carousel",
+      autogenerate: {
+        directory: "carousel"
+      }
+    }, {
+      label: "Checkbox",
+      autogenerate: {
+        directory: "checkbox"
+      }
+    }, {
+      label: "Chips",
+      autogenerate: {
+        directory: "chips"
+      }
+    }, {
+      label: "Pickers",
+      autogenerate: {
+        directory: "pickers"
+      }
+    }, {
+      label: "Dialogs",
+      autogenerate: {
+        directory: "dialogs"
+      }
+    }, {
+      label: "Divider",
+      autogenerate: {
+        directory: "divider"
+      }
+    }, {
+      label: "Lists",
+      autogenerate: {
+        directory: "lists"
+      }
+    }, {
+      label: "Menú",
+      autogenerate: {
+        directory: "menu"
+      }
+    }, {
+      label: "Navigation",
+      autogenerate: {
+        directory: "navigation"
+      }
+    }, {
+      label: "Progress indicator",
+      autogenerate: {
+        directory: "progress-indicator"
+      }
+    }, {
+      label: "Sheets",
+      autogenerate: {
+        directory: "sheets"
+      }
+    }, {
+      label: "Sliders",
+      autogenerate: {
+        directory: "sliders"
+      }
+    }, {
+      label: "Snackbar",
+      autogenerate: {
+        directory: "snackbar"
+      }
+    }, {
+      label: "Switch",
+      autogenerate: {
+        directory: "switch"
+      }
+    }, {
+      label: "Tabs",
+      autogenerate: {
+        directory: "tabs"
+      }
+    }, {
+      label: "Text",
+      autogenerate: {
+        directory: "text"
+      }
+    }, {
+      label: "Text fields",
+      autogenerate: {
+        directory: "text-fields"
+      }
+    }, {
+      label: "ToolTips",
+      autogenerate: {
+        directory: "tooltips"
+      }
+    }]
+  }), tailwind()]
 });
