cbff7002360a48d0951d6c0169248fc6ca890ada
AristiDevs <aris.guimera@gmail.com>
2023-12-26 07:36:27 +0000

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jetpackcompose.pro',
   integrations: [
     starlight({
       head: [
