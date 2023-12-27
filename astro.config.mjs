bdd200b75083167ff11b707d10a3bd40dfc6f729
AristiDevs <aris.guimera@gmail.com>
2023-12-27 18:58:38 +0000

    images

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jetpackcompose.pro',
+  site: 'https://www.jetpackcompose.pro',
   integrations: [
     starlight({
       head: [
